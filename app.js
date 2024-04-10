f676e90165b6f89a3d91a093a5be27b7b22fa190
Taiwo555 <rolandhunjunude60@gmail.com>
2024-04-10 12:14:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => console.log(`server on port ${PORT}`));
+const io = require('socket.io')(server);
+
+//dirman the current directory
+app.use(express.static(path.join(__dirname,  'public')))
+
+let socketsConnected = new Set()
+
+io.on('connection',onConnected)
+
+ function onConnected(socket) {
+    console.log(socket.id)
+    socketsConnected.add(socket)
+
+
+    socket.on('disconnect',() =>{
+        console.log('Socket disconnected', socket.id)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total',socketsConnected.size)
+    })
+    io.emit('clients-total',socketsConnected.size)
+    socket.on('message',(data) =>{
+        console.log(data)
+        socket.broadcast.emit('chat-message', data)
+     })
+
+     socket.on('feedback',(data) =>{
+        socket.broadcast.emit('feedback', data)
+     })
+ }
