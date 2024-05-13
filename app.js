9550e91cb9cf6bcc07498aed4da47fe00773661a
Odun Code <110565192+Taiwo555@users.noreply.github.com>
2024-05-13 13:39:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const io = require('socket.io')(server);
 //dirman the current directory
 app.use(express.static(path.join(__dirname,  'public')))
 
-let socketsConnected = new Set()
+let socketsConnected = new Set() //Initializing
 
 io.on('connection',onConnected)
 
@@ -21,12 +21,18 @@ io.on('connection',onConnected)
         console.log('Socket disconnected', socket.id)
         socketsConnected.delete(socket.id)
         io.emit('clients-total',socketsConnected.size)
-    })
+    })  //This code handles client disconnections. 
+    //It logs the event, updates the server's internal state to reflect the reduced number of connected clients,
+    // and broadcasts the updated client count to all remaining clients
+    
     io.emit('clients-total',socketsConnected.size)
     socket.on('message',(data) =>{
         console.log(data)
         socket.broadcast.emit('chat-message', data)
-     })
+     }) //This code It keeps track of connected users,
+        // listens for incoming messages from clients, 
+        //and broadcasts those messages to all other connected clients
+     
 
      socket.on('feedback',(data) =>{
         socket.broadcast.emit('feedback', data)
